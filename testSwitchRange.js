50467bb5dbe50448bd918eec90f45abd014af4bb
ryanjanelli <58485173+ryanjanelli@users.noreply.github.com>
2019-12-13 08:19:07 -0500

    Create testSwitchRange.js
    trying to figure out why my switch will not accept a range as the case.

---
+++ testSwitchRange.js
@@ -0,0 +1,23 @@
+// switch practice 
+
+var mathTestScores = [90, 72, 88, 64, 92];
+
+for (let i = 0; i < mathTestScores.length; i++) {
+    console.log(mathTestScores[i]);
+    switch (mathTestScores[i]) {
+            case (mathTestScores[i] >= 90) :
+                console.log('You got an A with a score of ' + mathTestScores[i] + '.');
+                break;
+            case (mathTestScores[i] >= 80 && mathTestScores[i] < 90) :
+                console.log('You got a B with a score of ' + mathTestScores[i] + '.');
+                break;
+            case (mathTestScores[i] >= 70 && mathTestScores[i] < 80) :
+                console.log('You got a C with a score of ' + mathTestScores[i] + '.');
+                break;
+            case (mathTestScores[i] >= 60 && mathTestScores[i] < 70) :
+                console.log('You got a D with a score of ' + mathTestScores[i] + '.');
+                break;
+            default :
+            console.log('You failed. A ' + mathTestScores[i] + ' is not good enough.');
+    }
+}
