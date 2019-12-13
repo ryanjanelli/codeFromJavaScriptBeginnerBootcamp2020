3dd9ea2c565b57d0d9dc3dc18ffc05fbd22722b3
ryanjanelli <58485173+ryanjanelli@users.noreply.github.com>
2019-12-13 13:34:34 -0500

    Add files via upload

---
+++ packingList.js
@@ -0,0 +1,42 @@
+var carryOnSuitcase = {
+    brand : 'Samsonite',
+    color : 'blue',
+    'Length in Inches' : 22,
+    'Width in Inches' : 14,
+    'Height in Inches' : 8,
+    'Has four wheels' : true,
+    contents : true,
+
+};
+
+carryOnSuitcase.contents = {
+    longSleeves : 3,
+    overnightBag : 1,
+    socks : 4,
+    undies : 4, 
+    tankShirt : 2,
+    belt : 1,
+    hotelReceipt : 1,
+    dirtyClothes : true,
+};
+
+var overnightBag = {
+    toothbrush : 1,
+    toothpaste : 1,
+    flossers : 4,
+    beardBrush : 1,
+    beardOil : 1,
+    comb : 1,
+};
+
+var backpack = {};
+backpack.laptopComputer = 1;
+backpack.laptopCharger = 1;
+backpack.folder = 1;
+backpack['Reusable plastic bags'] = 5;
+backpack['Microfiber cloth for glasses'] = 1;
+backpack.pens = 'Several';
+backpack['Phone Outlet Charger'] = 1;
+backpack['Rechargeable Battery Pack'] = 1;
+backpack.snacks = true;
+backpack.mints = 'Several';
