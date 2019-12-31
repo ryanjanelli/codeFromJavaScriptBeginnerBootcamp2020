4feab4c81d48f83c657aa39ee6d6edf2e1a3d865
ryanjanelli <ryanjanelli@gmail.com>
2019-12-31 11:18:54 -0500

    practicing template literals with multi line strings

---
+++ ES6tools/templateLiteralsPart2.js
@@ -0,0 +1,10 @@
+// practice creating multi line strings using ES5 and ES6 notation
+
+var multiLineString1 = 'look at me\n\
+i\'m on two lines';
+
+var multiLineString2 = `look at me
+i'm also on two lines`;
+
+console.log(multiLineString1);
+console.log(multiLineString2);
