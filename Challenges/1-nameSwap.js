0ac40131119cdc7204b3c973e8af461c3a8bd46d
ryanjanelli <ryanjanelli@gmail.com>
2020-01-07 08:04:09 -0500

    name swap challenge using fat arrow done two different ways

--- Challenges/1-nameSwap.js
+++ Challenges/1-nameSwap.js
@@ -1,11 +1,9 @@
 // take a given name and switch the position of the first name and last name
 // Example: "Abraham Lincoln" becomes "Lincoln Abraham"
 
-function nameSwap1(str) {
-    return str.split(' ').reverse().join(' ');
-}
+nameSwap1 = (str) => str.split(' ').reverse().join(' ');
 
-function nameSwap2(str) {
+nameSwap2 = (str) => {
     let strArray = str.split(' ');
     return `${strArray[1]} ${strArray[0]}`;
 }
