e91eb638f9078bce2d112edc68d3529039f41f12
ryanjanelli <ryanjanelli@gmail.com>
2020-01-07 08:29:08 -0500

    added comments to the name swap

--- Challenges/1-nameSwap.js
+++ Challenges/1-nameSwap.js
@@ -1,8 +1,13 @@
 // take a given name and switch the position of the first name and last name
 // Example: "Abraham Lincoln" becomes "Lincoln Abraham"
 
+// 1. split the string into an array of words
+// 2. reverse the order of the array 
+// 3. join it back together into a string.
 nameSwap1 = (str) => str.split(' ').reverse().join(' ');
 
+// 1. split the string into an array of words
+// 2. call the second item in the array followed by the first
 nameSwap2 = (str) => {
     let strArray = str.split(' ');
     return `${strArray[1]} ${strArray[0]}`;
