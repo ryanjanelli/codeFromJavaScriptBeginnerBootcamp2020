fcb33f4d782822462229b1b553c90cf2a4473e25
ryanjanelli <ryanjanelli@gmail.com>
2020-01-09 10:05:53 -0500

    remove renamed files

--- Challenges/8-reverseStringSpreadOperator.js
+++
@@ -1,8 +0,0 @@
-// reverse a string using the spread operator
-
-// turn str into an array using spread operator by putting it within brackets
-// reverse the array and join the array back into a string
-revString = (str) => [...str].reverse().join('');
-
-console.log(revString('hello'))
-console.log(revString('good morning'));
