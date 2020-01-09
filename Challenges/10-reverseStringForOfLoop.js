eaf9bc163591329132fe674a14e1ecb167680035
ryanjanelli <ryanjanelli@gmail.com>
2020-01-09 09:33:51 -0500

    added second function to reverse the string

--- Challenges/10-reverseStringForOfLoop.js
+++ Challenges/10-reverseStringForOfLoop.js
@@ -12,5 +12,20 @@ revString = (str) => {
     return rev;
 }
 
+// function that takes a string as an argument
+revString2 = (str) => {
+    let revArray = [];
+    // iterate though each char of the str
+    for (let char of str) {
+        // unshift each char to the front of the array
+        revArray.unshift(char);
+    }
+    // return the the array as a string
+    return revArray.join('');
+}
+
 console.log(revString('baloon'));
 console.log(revString('good morning'));
+
+console.log(revString2('baloon'));
+console.log(revString2('good morning'));
